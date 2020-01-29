5bd171d77510a7eb9f22cb31831798086c9d9eaf
Vito336 <56296791+Vito336@users.noreply.github.com>
2020-01-29 19:33:29 +0300

    Начала точек, добавляю слайдер-иннер, борюсь с шириной

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,8 @@ $(function(){
 		infinite: true,
 		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/slider-arrows-left.svg" alt=""></img>',
 		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/slider-arrows-right.svg" alt=""></img>',
+		dots: true,
+		dotsClass: 'slider-dots'
 	});
 
 });
