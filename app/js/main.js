f33bc8275e46c923511add21aa6835ef8483b0e3
Vito336 <56296791+Vito336@users.noreply.github.com>
2020-02-08 10:24:20 +0700

    Делаю якорную стрелку вверх

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
 	$('.reviews-slider').slick({
 		infinite: true,
@@ -6,6 +6,15 @@ $(function(){
 		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/slider-arrows-right.svg" alt=""></img>',
 		dots: true,
 		dotsClass: 'slider-dots'
+	});
+
+	$(document).ready(function () {
+		$("#arrow-top").on("click", "a", function (event) {
+			event.preventDefault();
+			var id = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({ scrollTop: top }, 500);
+		});
 	});
 
 });
